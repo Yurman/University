985479b48afb7d5c5a4b9dd311a6fb9bff8bbfe2
Yurii Opanasenko <yurman04@gmail.com>
2019-07-28 23:07:22 +0300

    Wrote methods for getting schedule in Schedule class

--- src/main/java/com/foxminded/university/Schedule.java
+++ src/main/java/com/foxminded/university/Schedule.java
@@ -17,16 +17,30 @@ public class Schedule {
 
 	public List<ScheduleItem> getProfessorSchedule(Professor professor, LocalDate startDate, LocalDate endDate) {
 		List<ScheduleItem> professorSchedule = new ArrayList<ScheduleItem>();
+		for (ScheduleItem item : schedule) {
+			if ((item.getProfessor().equals(professor))
+					&& ((item.getDate().isAfter(startDate)) || item.getDate().isEqual(startDate))
+					&& ((item.getDate().isBefore(endDate)) || item.getDate().isEqual(endDate))) {
+				professorSchedule.add(item);
+			}
+		}
+
 		return professorSchedule;
 	}
 
 	public List<ScheduleItem> getGroupSchedule(Group group, LocalDate startDate, LocalDate endDate) {
 		List<ScheduleItem> groupSchedule = new ArrayList<ScheduleItem>();
+		for (ScheduleItem item : schedule) {
+			if ((item.getGroups().contains(group))
+					&& ((item.getDate().isAfter(startDate)) || item.getDate().isEqual(startDate))
+					&& ((item.getDate().isBefore(endDate)) || item.getDate().isEqual(endDate))) {
+				groupSchedule.add(item);
+			}
+		}
 		return groupSchedule;
 	}
 
 	public List<ScheduleItem> getStudentSchedule(Student student, LocalDate startDate, LocalDate endDate) {
-		List<ScheduleItem> studentSchedule = new ArrayList<ScheduleItem>();
-		return studentSchedule;
+		return getGroupSchedule(student.getGroup(), startDate, endDate);
 	}
 }

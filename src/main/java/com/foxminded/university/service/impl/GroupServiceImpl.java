f3995c5229e12ddb33ff06965e307fe4323a20e9
Yurii Opanasenko <yurman04@gmail.com>
2019-12-16 23:39:23 +0300

    Modified service classes

--- src/main/java/com/foxminded/university/service/impl/GroupServiceImpl.java
+++ src/main/java/com/foxminded/university/service/impl/GroupServiceImpl.java
@@ -2,37 +2,41 @@ package com.foxminded.university.service.impl;
 
 import java.util.List;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.foxminded.university.config.DataConfiguration;
-import com.foxminded.university.dao.impl.GroupDaoImpl;
+import com.foxminded.university.dao.GroupDao;
 import com.foxminded.university.domain.Group;
 import com.foxminded.university.service.GroupService;
 
-@Component
+@Service
 public class GroupServiceImpl implements GroupService {
-    private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-            DataConfiguration.class);
-    private GroupDaoImpl groupDao = context.getBean(GroupDaoImpl.class);
 
+    @Autowired
+    private GroupDao groupDao;
+
+    @Override
     public Group addGroup(Group group) {
         return groupDao.add(group);
     }
 
+    @Override
     public Group updateGroup(Group group) {
         return groupDao.update(group);
     }
 
-    public boolean deleteSGroup(Group group) {
-        return groupDao.delete(group.getId());
+    @Override
+    public boolean deleteGroup(int id) {
+        return groupDao.delete(id);
     }
 
-    public Group getStudentById(Group group) {
-        return groupDao.getById(group.getId());
+    @Override
+    public Group getGroupById(int id) {
+        return groupDao.getById(id);
     }
 
-    public List<Group> getAllGroups() {
+    @Override
+    public List<Group> Groups() {
         return groupDao.getAll();
     }
 }
--- src/main/java/com/foxminded/university/service/impl/StudentServiceImpl.java
+++ src/main/java/com/foxminded/university/service/impl/StudentServiceImpl.java
@@ -2,36 +2,40 @@ package com.foxminded.university.service.impl;
 
 import java.util.List;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.foxminded.university.config.DataConfiguration;
-import com.foxminded.university.dao.impl.StudentDaoImpl;
+import com.foxminded.university.dao.StudentDao;
 import com.foxminded.university.domain.Student;
 import com.foxminded.university.service.StudentService;
 
-@Component
+@Service
 public class StudentServiceImpl implements StudentService {
-    private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-            DataConfiguration.class);
-    private StudentDaoImpl studentDao = context.getBean(StudentDaoImpl.class);
 
+    @Autowired
+    private StudentDao studentDao;
+
+    @Override
     public Student addStudent(Student student) {
         return studentDao.add(student);
     }
 
+    @Override
     public Student updateStudent(Student student) {
         return studentDao.update(student);
     }
 
-    public boolean deleteStudent(Student student) {
-        return studentDao.delete(student.getId());
+    @Override
+    public boolean deleteStudent(int id) {
+        return studentDao.delete(id);
     }
 
-    public Student getStudentById(Student student) {
-        return studentDao.getById(student.getId());
+    @Override
+    public Student getStudentById(int id) {
+        return studentDao.getById(id);
     }
 
+    @Override
     public List<Student> getAllStudents() {
         return studentDao.getAll();
     }

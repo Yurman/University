9a99bc3c9542cd6f3e6aa16c8f62d395f95bde27
Yurii Opanasenko <yurman04@gmail.com>
2020-04-07 11:08:29 +0300

    Modified DataConfiguration to work with DataSource

--- src/main/java/com/foxminded/university/config/DataConfiguration.java
+++ src/main/java/com/foxminded/university/config/DataConfiguration.java
@@ -1,35 +1,30 @@
 package com.foxminded.university.config;
 
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
 
 @Configuration
 @ComponentScan("com.foxminded.university")
-@PropertySource("classpath:application.properties")
 public class DataConfiguration {
 
-    @Autowired
-    private Environment environment;
-
-    private final String URL = "db.url";
-    private final String USER = "db.user";
-    private final String DRIVER = "db.driver";
-    private final String PASSWORD = "db.password";
+    private static final Logger logger = LoggerFactory.getLogger(DataConfiguration.class);
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(environment.getProperty(URL));
-        dataSource.setUsername(environment.getProperty(USER));
-        dataSource.setPassword(environment.getProperty(PASSWORD));
-        dataSource.setDriverClassName(environment.getProperty(DRIVER));
+    DataSource dataSource() {
+        DataSource dataSource = null;
+        JndiTemplate jndi = new JndiTemplate();
+        try {
+            dataSource = (DataSource) jndi.lookup("java:comp/env/jdbc/university");
+        } catch (NamingException e) {
+            logger.error("Problem with finding DataSource", e);
+        }
         return dataSource;
     }
 }
--- src/main/java/com/foxminded/university/config/UniversityDataSource.java
+++
@@ -1,5 +0,0 @@
-package com.foxminded.university.config;
-
-public class UniversityDataSource {
-
-}

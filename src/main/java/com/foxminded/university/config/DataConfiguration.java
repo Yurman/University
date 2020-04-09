180f8737c8acbf3775217cf19867813c04ddabde
Yurii Opanasenko <yurman04@gmail.com>
2020-04-09 21:32:50 +0300

    Modified DataConfiguration class

--- src/main/java/com/foxminded/university/config/DataConfiguration.java
+++ src/main/java/com/foxminded/university/config/DataConfiguration.java
@@ -3,8 +3,6 @@ package com.foxminded.university.config;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,17 +19,9 @@ public class DataConfiguration {
     @Autowired
     private Environment environment;
 
-    private static final Logger logger = LoggerFactory.getLogger(DataConfiguration.class);
-
     @Bean
-    DataSource dataSource() {
-        DataSource dataSource = null;
-        try {
-            JndiTemplate template = new JndiTemplate();
-            dataSource = (DataSource) template.lookup(environment.getProperty("ds.name.context"));
-        } catch (NamingException e) {
-            logger.error("Problem with finding DataSource", e);
-        }
-        return dataSource;
+    DataSource dataSource() throws NamingException {
+
+        return (DataSource) new JndiTemplate().lookup(environment.getProperty("ds.name.context"));
     }
 }
